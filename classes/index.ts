025167e79cb46a1205a77e0c43bf20b40a4981cd
Yousef Gamal <youseffgamal123@gmail.com>
2023-04-10 01:19:36 +0200

    ignored implementation class

--- classes/index.ts
+++ classes/index.ts
@@ -1,7 +1,6 @@
 import MoodleAttempt from './MoodleAttempt';
 import MoodleAttemptUpdate from './MoodleAttemptUpdate';
 import MoodleApi, { MoodleClient } from './MoodleClient';
-import MoodleQMultiChoice from './MoodleQMultiChoice';
 import MoodleQuestion from './MoodleQuestion';
 
 export {
@@ -9,6 +8,5 @@ export {
   MoodleAttempt,
   MoodleAttemptUpdate,
   MoodleClient,
-  MoodleQMultiChoice,
   MoodleQuestion,
 };
