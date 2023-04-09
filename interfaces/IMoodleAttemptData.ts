800f71a42981d733366e51d14f2d8f269d12cab1
Yousef Gamal <youseffgamal123@gmail.com>
2023-04-10 00:37:15 +0200

    change warnings type

--- interfaces/IMoodleAttemptData.ts
+++ interfaces/IMoodleAttemptData.ts
@@ -1,10 +1,11 @@
 import IMoodleAttempt from './IMoodleAttempt';
 import IMoodleQuestion from './IMoodleQuestion';
+import IMoodleWSWarning from './IMoodleWSWarning';
 
 export default interface IMoodleAttemptData {
   attempt: IMoodleAttempt;
   messages: unknown[];
   nextpage: number;
   questions: IMoodleQuestion[];
-  warnings: unknown[];
+  warnings: IMoodleWSWarning[];
 }
