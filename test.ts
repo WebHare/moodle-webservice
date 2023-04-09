c10e3fdaa06d7c904c8aab8e7dfe98d620406ec9
Yousef Gamal <youseffgamal123@gmail.com>
2023-04-10 01:20:13 +0200

    test

--- test.ts
+++ test.ts
@@ -1,15 +1,20 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import MoodleApi, { MoodleClient, MoodleAttempt } from '.';
-import utils from 'util';
+import MoodleApi, { MoodleQuestion } from '.';
 
 const moodle = MoodleApi({
   baseUrl: 'http://aunonline.aun.edu.eg/med-ns/',
   token: '3752087c24677b2960cc03b821bd27cd',
 });
 
-moodle.mod.quiz
-  .getAttemptReview({ attemptid: 3665103 })
-  .then((res) =>
-    console.log(utils.inspect(MoodleAttempt.parse(res), { depth: null }))
-  );
+moodle.mod.quiz.getAttemptReview({ attemptid: 3665103 }).then((res) => {
+  const questions = res.questions; //<-- of type IMoodleQuestion[].
+  //IMoodleQuestion does not contain the actual question text or choices.
+  questions[0].text; //<-- Error no such property on questions
+
+  //IMoodleParsedQuestion contains additional data such as the name, instance number and choices
+  const parsedQuestion = MoodleQuestion.parse(questions[0]); //<-- of type IMoodleParsedQuestion.
+
+  console.log(parsedQuestion.text);
+  //Output: "The muscles of facial expression are supplied by the ____ nerve."
+});
